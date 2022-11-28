494aed88bd33318641b5c234814da14488bb35b1
gvmgonsptech <marcus.goncalves@sptech.school>
2022-11-28 15:48:27 -0300

    Finalização da tela comunidade, login e cadastro + funcionalidades de validação

--- site/public/js/funcoes.js
+++ site/public/js/funcoes.js
@@ -2,18 +2,34 @@
 function validarSessao() {
     // aguardar();
 
-    var email = sessionStorage.EMAIL_USUARIO;
-    var nome = sessionStorage.NOME_USUARIO;
+    var username = sessionStorage.NAME_USER;
+    var idUser = sessionStorage.ID_USER;
+    var element = sessionStorage.ELEMENT_USER
 
-    var b_usuario = document.getElementById("b_usuario");
+    var showUsername = document.getElementById("showUsername");
 
-    if (email != null && nome != null) {
+    if (username != null && idUser != null && element != null) {
         // window.alert(`Seja bem-vindo, ${nome}!`);
-        b_usuario.innerHTML = nome;
+        showUsername = username;
 
         // finalizarAguardar();
     } else {
         window.location = "../login.html";
+    }
+}
+
+function validarSessaoComunidade() {
+    // aguardar();
+
+    var username = sessionStorage.NAME_USER;
+    var idUser = sessionStorage.ID_USER;
+    var element = sessionStorage.ELEMENT_USER
+
+    var showUsername = document.getElementById("showUsername");
+
+    if (username != null && idUser != null && element != null) {
+        window.location = "../comunidade.html";
+    } else {
     }
 }
 
@@ -25,19 +41,24 @@ function limparSessao() {
 }
 
 // carregamento (loading)
-function aguardar() {
-    var divAguardar = document.getElementById("div_aguardar");
+function aguardarRegister() {
+    var divAguardar = document.getElementById("loadingRegister");
     divAguardar.style.display = "flex";
 }
 
-function finalizarAguardar(texto) {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "none";
+function aguardarLogin() {
+    var divAguardar = document.getElementById("loadingLogin");
+    divAguardar.style.display = "flex";
+}
 
-    var divErrosLogin = document.getElementById("div_erros_login");
-    if (texto) {
-        divErrosLogin.innerHTML = texto;
-    }
+function finalizarAguardarRegister() {
+    var divAguardar = document.getElementById("loadingRegister");
+    divAguardar.style.display = "none";
+}
+
+function finalizarAguardarLogin() {
+    var divAguardar = document.getElementById("loadingLogin");
+    divAguardar.style.display = "none";
 }
 
 
